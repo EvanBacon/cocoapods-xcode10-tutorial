1f845076a8d6b940295528be056489e6e36852c3
Evan Bacon <baconbrix@gmail.com>
2018-10-09 12:34:05 -0700

    Initial Commit

---
+++ somethingcool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  somethingcool
+//
+//  Created by Evan Bacon on 10/9/18.
+//  Copyright © 2018 Evan Bacon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
